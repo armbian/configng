1e974d2c56e6c312f68d97f6e10a7bafff14b2ac
Joey Turner <trarran@---.com>
2023-08-11 22:33:06 -0700

    renamed:    DEBIAN/control -> DEBIAN/DEBIAN_config/control 	renamed:    DEBIAN/postinst -> DEBIAN/DEBIAN_config/postinst 	renamed:    DEBIAN/postrm -> DEBIAN/DEBIAN_config/postrm 	renamed:    DEBIAN/prerm -> DEBIAN/DEBIAN_config/prerm 	new file:   DEBIAN/DEBIAN_monitor/control 	new file:   DEBIAN/DEBIAN_monitor/postinst 	new file:   DEBIAN/DEBIAN_monitor/postrm 	new file:   DEBIAN/DEBIAN_monitor/prerm 	new file:   DEBIAN/DEBIAN_tui/control 	new file:   DEBIAN/DEBIAN_tui/postinst 	new file:   DEBIAN/DEBIAN_tui/postrm 	new file:   DEBIAN/DEBIAN_tui/prerm 	modified:   makedeb.sh

--- makedeb.sh
+++ makedeb.sh
@@ -1,32 +1,59 @@
 #!/bin/bash
+
+clear
 directory="$(dirname "$(readlink -f "$0")")"
-cd $directory ;
+cd "${directory}" || exit;
+
+debdir="${directory}/debs" ;
+tmpddir="/tmp/build" ;
+
 
 
 codename=$( grep VERSION_CODENAME /etc/os-release | cut -d"="  -f2 )
-catagory=$( grep Section DEBIAN/control | cut -d" "  -f2 )
-version=$( grep Version DEBIAN/control | cut -d" "  -f2 )
-debname=$( echo "armbian-${codename}-${catagory}-config.${version}" )
-debdir="$directory/debs" ;
-tmpddir="/tmp/build" ;
 
 # Loop through files in the bin/ directory
-for file in $directory/usr/bin/; do
+for file in "$directory"/usr/bin/*; do
+    filename=$(basename "$file")
+    
+    debname=$( printf '%s' "armbian-${codename}-${catagory}-${filename}.${version}" )
     # Check if the file is not named config
     if [[ "$(basename "$file")" != "config" ]]; then
+        
         # Perform an action for this file
-        echo "Processing file: $file"
+        echo -e "Processing file: $file"
+
+        [[ ! -d "${tmpddir}" ]] && mkdir -p "${tmpddir}/usr/bin" ;
+        [[ ! -d "${directory}/debs/" ]] && mkdir -p "${directory}/debs/" ;
+
+        cp -r "${directory}/DEBIAN/DEBIAN_${filename}" "${tmpddir}/DEBIAN" ;
+        cp "${directory}/usr/bin/${filename}" "${tmpddir}/usr/bin/" ;
+        echo "${directory}/usr/bin/${filename}" "${tmpddir}/usr/bin/" ;
+
+        catagory=$( grep Section ${tmpddir}/DEBIAN/control | cut -d" "  -f2 )
+        version=$( grep Version ${tmpddir}/DEBIAN/control | cut -d" "  -f2 )
+
+
+        dpkg-deb --build "${tmpddir}" "${directory}/debs/${debname}.deb"
+        echo -e "\n"
+        dpkg-deb -I "${debdir}/${debname}.deb" ;
+        echo
+        dpkg-deb -c "${debdir}/${debname}.deb" ;
+
+        [[ -d $tmpddir ]] && rm -r "${tmpddir}" ;
     fi
 done
 
-[[ ! -d $tmpddir ]] && mkdir -p /tmp/build/usr/ ;
-[[ ! -d $directory/debs/ ]] && mkdir -p "$directory/debs/" ;
 
-cp -r "$directory/DEBIAN/" /tmp/build/ ;
-cp -r "$directory/usr/bin/" /tmp/build/usr/bin/ ;
-cp -r "$directory/usr/lib/" /tmp/build/usr/lib/ ;
+build_all(){
 
-dpkg-deb --build /tmp/build "$directory/debs/$debname.deb"
+[[ ! -d "$tmpddir" ]] && mkdir -p "$tmpddir/usr/" ;
+[[ ! -d "$directory/debs/" ]] && mkdir -p "$directory/debs/" ;
+
+cp -r "$directory/DEBIAN/" "$tmpddir" ;
+cp -r "$directory/usr/bin/" "$tmpddir"/usr/bin/ ;
+cp -r "$directory/usr/lib/" "$tmpddir"/usr/lib/ ;
+
+dpkg-deb --build "$tmpddir" "$directory/debs/$debname.deb"
 echo -e "\n"
 dpkg-deb -I "$debdir/$debname.deb" ;
 echo
@@ -34,3 +61,4 @@ dpkg-deb -c "$debdir/$debname.deb" ;
 
 [[ -d $tmpddir ]] && rm -r "$tmpddir" ;
 
+}
