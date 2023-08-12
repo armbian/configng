3bdce9e2499130927a1ed400294f51c96e89231b
Joey Turner <trarran@---.com>
2023-08-11 21:18:36 -0700

    modified:   makedeb.sh

--- makedeb.sh
+++ makedeb.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 directory="$(dirname "$(readlink -f "$0")")"
-cd $directory || echo "You seem to be in the correct folder" ;
+cd $directory ;
+
+
 codename=$( grep VERSION_CODENAME /etc/os-release | cut -d"="  -f2 )
 catagory=$( grep Section DEBIAN/control | cut -d" "  -f2 )
 version=$( grep Version DEBIAN/control | cut -d" "  -f2 )
 debname=$( echo "armbian-${codename}-${catagory}-config.${version}" )
+debdir="$directory/debs" ;
+tmpddir="/tmp/build" ;
 
 # Loop through files in the bin/ directory
-for file in bin/*; do
+for file in $directory/usr/bin/; do
     # Check if the file is not named config
     if [[ "$(basename "$file")" != "config" ]]; then
         # Perform an action for this file
@@ -15,13 +19,18 @@ for file in bin/*; do
     fi
 done
 
-[[ ! -d /tmp/build ]] && mkdir -p /tmp/build/usr/ ;
-[[ ! -d $directory/debs/ ]] && mkdir -p $directory/debs/ ;
+[[ ! -d $tmpddir ]] && mkdir -p /tmp/build/usr/ ;
+[[ ! -d $directory/debs/ ]] && mkdir -p "$directory/debs/" ;
 
-cp -r $directory/DEBIAN/ /tmp/build/ ;
-cp -r $directory/usr/bin/ /tmp/build/usr/bin/ ;
-cp -r $directory/usr/lib/ /tmp/build/usr/lib/ ;
-dpkg-deb --build /tmp/build $directory/debs/$debname.deb
-dpkg-deb --contents $directory/debs/$debname.deb
+cp -r "$directory/DEBIAN/" /tmp/build/ ;
+cp -r "$directory/usr/bin/" /tmp/build/usr/bin/ ;
+cp -r "$directory/usr/lib/" /tmp/build/usr/lib/ ;
 
-[[ -d /tmp/build ]] && rm -r /tmp/build ;
+dpkg-deb --build /tmp/build "$directory/debs/$debname.deb"
+echo -e "\n"
+dpkg-deb -I "$debdir/$debname.deb" ;
+echo
+dpkg-deb -c "$debdir/$debname.deb" ;
+
+[[ -d $tmpddir ]] && rm -r "$tmpddir" ;
+
