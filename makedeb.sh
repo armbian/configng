1c57dd1ce427eebc38b41832e41f807f30b8addc
Joey Turner <trarran@---.com>
2023-08-11 23:43:48 -0700

    modified:   DEBIAN/DEBIAN_monitor/control 	deleted:    DEBIAN/DEBIAN_monitor/postinst 	deleted:    DEBIAN/DEBIAN_monitor/postrm 	deleted:    DEBIAN/DEBIAN_monitor/prerm 	deleted:    DEBIAN/DEBIAN_tui/postinst 	deleted:    DEBIAN/DEBIAN_tui/postrm 	deleted:    DEBIAN/DEBIAN_tui/prerm 	modified:   makedeb.sh

--- makedeb.sh
+++ makedeb.sh
@@ -6,50 +6,80 @@ cd "${directory}" || exit;
 
 debdir="${directory}/debs" ;
 tmpddir="/tmp/build" ;
-
-
-
 codename=$( grep VERSION_CODENAME /etc/os-release | cut -d"="  -f2 )
 
+build_debs(){
 # Loop through files in the bin/ directory
 for file in "$directory"/usr/bin/*; do
-    filename=$(basename "$file")
-    
-    debname=$( printf '%s' "armbian-${codename}-${catagory}-${filename}.${version}" )
+
+    filename=$(basename "$file") 
+
+    [[ ! -d "${tmpddir}" ]] && mkdir -p "${tmpddir}/usr/bin" ;
+    [[ ! -d "${directory}/debs/" ]] && mkdir -p "${directory}/debs/" ;
+
     # Check if the file is not named config
     if [[ "$(basename "$file")" != "config" ]]; then
         
         # Perform an action for this file
         echo -e "Processing file: $file"
-
-        [[ ! -d "${tmpddir}" ]] && mkdir -p "${tmpddir}/usr/bin" ;
-        [[ ! -d "${directory}/debs/" ]] && mkdir -p "${directory}/debs/" ;
-
         cp -r "${directory}/DEBIAN/DEBIAN_${filename}" "${tmpddir}/DEBIAN" ;
-        cp "${directory}/usr/bin/${filename}" "${tmpddir}/usr/bin/" ;
-        echo "${directory}/usr/bin/${filename}" "${tmpddir}/usr/bin/" ;
-
         catagory=$( grep Section ${tmpddir}/DEBIAN/control | cut -d" "  -f2 )
         version=$( grep Version ${tmpddir}/DEBIAN/control | cut -d" "  -f2 )
-
-
+        debname=$( printf '%s' "armbian-${codename}-${catagory}-${filename}.${version}" )
+        cp "${directory}/usr/bin/${filename}" "${tmpddir}/usr/bin/" ;
+        echo "${directory}/usr/bin/${filename}" "${tmpddir}/usr/bin/" ;
+        
         dpkg-deb --build "${tmpddir}" "${directory}/debs/${debname}.deb"
         echo -e "\n"
         dpkg-deb -I "${debdir}/${debname}.deb" ;
         echo
         dpkg-deb -c "${debdir}/${debname}.deb" ;
+        echo
+        tree /tmp/build
+        echo
 
-        [[ -d $tmpddir ]] && rm -r "${tmpddir}" ;
+        elif [[ "$(basename "$file")" == "config" ]]; then
+        # Perform an action for the config file
+        # ...
+
+
+        build_all
+        else
+        # Handle any other cases
+        echo "Error: Unknown file"
+
     fi
+    
+    unset filename debname catagory version ;
+    [[ -d $tmpddir ]] && rm -r "${tmpddir}" ;
+
 done
 
-
+}
 build_all(){
+    # Perform an action for this file
+    echo -e "Processing file: $file"
+    cp -r "${directory}/DEBIAN/DEBIAN_${filename}" "${tmpddir}/DEBIAN" ;
+    catagory=$( grep Section ${tmpddir}/DEBIAN/control | cut -d" "  -f2 )
+    version=$( grep Version ${tmpddir}/DEBIAN/control | cut -d" "  -f2 )
+    debname=$( printf '%s' "armbian-${codename}-${catagory}-${filename}.${version}" )
+    cp "${directory}/usr/bin/${filename}" "${tmpddir}/usr/bin/" ;
+    cp -r "${directory}/usr/lib/${filename}" "${tmpddir}/usr/lib/" ;
+    echo "${directory}/usr/bin/${filename}" "${tmpddir}/usr/bin/" ;
 
-[[ ! -d "$tmpddir" ]] && mkdir -p "$tmpddir/usr/" ;
-[[ ! -d "$directory/debs/" ]] && mkdir -p "$directory/debs/" ;
+    dpkg-deb --build "${tmpddir}" "${directory}/debs/${debname}.deb"
+    echo -e "\n"
+    dpkg-deb -I "${debdir}/${debname}.deb" ;
+    echo
+    dpkg-deb -c "${debdir}/${debname}.deb" ;
+    echo
+    tree /tmp/build
+    echo
 
-cp -r "$directory/DEBIAN/" "$tmpddir" ;
+
+
+
+cp -r "$directory/DEBIAN/DEBIAN_config/" "$tmpddir/DEBIAN" ;
 cp -r "$directory/usr/bin/" "$tmpddir"/usr/bin/ ;
 cp -r "$directory/usr/lib/" "$tmpddir"/usr/lib/ ;
 
@@ -62,3 +92,5 @@ dpkg-deb -c "$debdir/$debname.deb" ;
 [[ -d $tmpddir ]] && rm -r "$tmpddir" ;
 
 }
+
+build_debs
