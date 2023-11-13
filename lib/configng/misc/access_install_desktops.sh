ebc143d5b3c6c4935525e86e4c8d18fb2a7368b5
Joey Turner <tearran@gmail.com>
2023-11-13 15:09:23 -0700

    modified:   bin/configng-interface 	deleted:    lib/build/lib/functions/cli/utils-cli.sh 	renamed:    lib/configng/cpu.sh -> lib/config/cpu.sh 	new file:   lib/configng/misc/access_install_desktops.sh

---
+++ lib/configng/misc/access_install_desktops.sh
@@ -0,0 +1,22 @@
+#
+# Copyright (c) Authors: http://www.armbian.com/authors, info@armbian.com
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+#  utility_install_desktops related functions. See *(todo)* for more info.
+
+# @description Display a list of avalible desktops to install.
+#
+# @exitcode 0  If successful.
+#
+# @options none
+function desktops::see_list(){
+
+	echo "One moment please, searching for Desktops." | armbian-interface -o ;
+	apt-cache search armbian desktop |
+	awk -F "- " '{print $1, $2 }'
+	return 0
+	
+	}
