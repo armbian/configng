1980641618a337579dc5cfd909f9e718f26ad2ca
joey.turner <tearran@gmail.com>
2023-11-27 02:46:45 -0700

    modified:   bin/armbian-build-doc 	renamed:    bin/armbian-config-dev -> bin/armbian-configng-dev 	modified:   lib/armbian-configng-dev/Misc/setup_dev_chroot.sh 	modified:   share/armbian-build/build-commands.html

--- lib/armbian-configng-dev/Misc/setup_dev_chroot.sh
+++ lib/armbian-configng-dev/Misc/setup_dev_chroot.sh
@@ -25,62 +25,47 @@ check_install() {
 function process_output() {
 
     while IFS= read -r line; do
-             clear
-        if [[ $line == I:* || $line == "Get:*" || $line == "Hit:*" ]]; then
-            echo -e "100\n$line" 
-            sleep 0.1
-            echo -e "0\n$line" 
-        fi | dialog --gauge "$line" 6 60 0       
-        # if the line starts with W: then it's a warning
-
-        if [[ $line == W:* ]]; then
-            dialog --title "Question" --yesno "Warning: $line Are you sure you want to proceed?" 7 60
-            response=$?
-            case $response in
-            0) echo "You chose Yes.";;
-            1) echo "You chose No.";;
-            255) echo "You chose Cancel.";;
-            esac    
         # if the line starts with O: then it's a success
-        elif [[ $line == O:* ]]; then
+        if [[ $line == O:* ]] || [[ $line == E:* ]] || [[ $line == W:* ]]; then
             echo -e "Complete: $line" | armbian-interface -o 
-        # if the line starts with E: then it's an error
-        elif [[ $line == E:* ]]; then
-            dialog --title "Question" --yesno "Warning: $line Are you sure you want to proceed?" 7 60   
-            response=$?
-            case $response in
-            0) echo "You chose Yes.";;
-            1) echo "You chose No.";;
-            255) echo "You chose Cancel." ; exit 0 ;;
-            esac    
-        fi
-   
+        elif [[ $line == I:* || $line == "Get:*" || $line == "Hit:*" ]]; then
+            echo -e "100\n$line" 
+            sleep 0.25
+            echo -e "0\n$line" 
+        fi | dialog --gauge "$line" 6 60 0        
     done 
 }
 # This will setup a chroot development environment.
 function setup_chroot() {
     
-{
+
     local config_repo="https://github.com/Tearran/configng.git"
     local debootstrap_reliase="jammy"
     local debootstrap_url="http://ports.ubuntu.com/ubuntu-ports"
     local chroot_dir=/opt/armbian
-    echo "I: Setting up chroot environment..."
-    [[ -d $chroot_dir ]] && echo "W: Found Yes to remove and continue" && sudo rm -rf $chroot_dir ; sleep 1   
-    sudo mkdir -p $chroot_dir
-    echo "I: Installing debootstrap..."
-    sudo debootstrap --variant=buildd jammy $chroot_dir $debootstrap_url 
+    
+    [[ -d $chroot_dir ]] && echo "E: Found $chroot_dir Remove it first" && return 1 ;
+    [[ ! -d "$chroot_dir" ]] && sudo mkdir -p "$chroot_dir" && sudo debootstrap --variant=buildd jammy $chroot_dir $debootstrap_url | process_output
+    
+    {
+    sudo rm -rf "$chroot_dir"
+	sudo mkdir -p "$chroot_dir"
+    echo "I: Installing debootstrap..." 
+    } | process_output
+    
     #mount -t proc proc $chroot_dir/proc   
-    echo "I: echo 'I: Installing git" ; 
+    {
+    echo "I: echo 'I: Installing git" 
     sudo chroot $chroot_dir apt update 
-
     echo "I: echo 'I: Installing git, this may take a while..." 
     sudo chroot $chroot_dir apt install -y git 
     echo "I: Cloning ConfigNG repository into chroot directory..." 
     sudo chroot $chroot_dir git clone $config_repo /opt/configng  
-} | process_output
-
-echo "O: Chroot environment setup complete." | process_output
+    } | process_output
+    
+    {
+    echo "O: Chroot environment setup complete."
+    } | process_output
 
 }
 
@@ -94,4 +79,5 @@ function chroot::setup(){
     # Set the directory for the container's root file system
     setup_chroot
     return 0
+    reset > /dev/null 2>&1
 }
