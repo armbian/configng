10f1ea8f7cd45e4e2cf46a277b6f7e9ef535cd42
Your Name <tearran@gmail.com>
2023-12-28 23:53:28 -0700

    modified:   lib/armbian-configng/locales/time_date.sh

--- lib/armbian-configng/locales/time_date.sh
+++ lib/armbian-configng/locales/time_date.sh
@@ -3,16 +3,25 @@
 # @exitcode 0  If successful.
 # @default none
 # @options none
-function locales::select_zone(){
-    echo "Please select a time zone:"
-    select zone in $(timedatectl list-timezones); do
-        if [[ -n "$zone" ]]; then
-            echo "You have selected the time zone: $zone"
-            timedatectl set-timezone "$zone"
-            break
+function locales::Timezone(){
+    echo "Please select a country:"
+    countries=$(timedatectl list-timezones | cut -d'/' -f1 | uniq)
+    select country in $countries; do
+        if [[ -n "$country" ]]; then
+            echo "You have selected the country: $country"
+            echo "Please select a time zone:"
+            zones=$(timedatectl list-timezones | grep "^$country/")
+            select zone in $zones; do
+                if [[ -n "$zone" ]]; then
+                    echo "You have selected the time zone: $zone"
+                    sudo timedatectl set-timezone "$zone"
+                    break 2
+                else
+                    echo "Invalid selection"
+                fi
+            done
         else
             echo "Invalid selection"
         fi
     done
-
 }
