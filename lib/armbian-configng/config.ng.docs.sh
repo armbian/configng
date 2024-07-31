88068b89603586e57de1c3284f8067475e9de7be
schwar3kat <61094841+schwar3kat@users.noreply.github.com>
2024-08-01 00:00:11 +1200

    3rd party Debian based
     distributions script must remain unexpanded in the README.md file

--- lib/armbian-configng/config.ng.docs.sh
+++ lib/armbian-configng/config.ng.docs.sh
@@ -51,7 +51,7 @@ sudo apt install armbian-config
     sudo wget https://apt.armbian.com/armbian.key -O key
     sudo gpg --dearmor < key | sudo tee /usr/share/keyrings/armbian.gpg > /dev/null
     sudo chmod go+r /usr/share/keyrings/armbian.gpg
-    sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/armbian.gpg] http://apt.armbian.com $(lsb_release -cs) main  $(lsb_release -cs)-utils  $(lsb_release -cs)-desktop" | sudo tee /etc/apt/sources.list.d/armbian.list
+    sudo echo "deb [arch=\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/armbian.gpg] http://apt.armbian.com \$(lsb_release -cs) main  \$(lsb_release -cs)-utils  \$(lsb_release -cs)-desktop" | sudo tee /etc/apt/sources.list.d/armbian.list
     sudo apt update
     sudo apt install armbian-config
 }
