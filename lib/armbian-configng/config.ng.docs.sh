cdbf1ce02b269d597f6d333bfd2b08b2d90a5815
Tearran <tearran@armbian.com>
2024-09-08 18:57:37 -0500

    fix: --help to display more nested items

--- lib/armbian-configng/config.ng.docs.sh
+++ lib/armbian-configng/config.ng.docs.sh
@@ -488,10 +488,11 @@ EOF
     # TODO: Migrate More features. 
     #echo " main=help   -  Display Legacy cli commands."
     jq -r --arg script_name "$script_name" '
-        .menu[] | 
-        .sub[] | 
-        select(.id | startswith("H") | not) |
-       "    --cli " + .id + "  -  " + .description
+        def process_item(item):
+	    "    --cli " + item.id + "  -  " + item.description,
+	    (item.sub[]? | process_item(.));
+	    .menu[] |
+	    .sub[]? | process_item(.)
     ' $json_file
 }
 
