06ed1efb161a22b67a9e486ce6a0dccc4d917f1f
Tearran <tearran@armbian.com>
2024-09-08 23:28:40 -0500

    feat: Added tui prompt.

--- lib/armbian-configng/config.ng.functions.sh
+++ lib/armbian-configng/config.ng.functions.sh
@@ -564,7 +564,43 @@ module_options+=(
 #
 # Function to execute the command
 #
+
 function execute_command() {
+    local id=$1
+
+    # Extract commands
+    local commands=$(jq -r --arg id "$id" '
+      .menu[] | 
+      .. | 
+      objects | 
+      select(.id == $id) | 
+      .command[]?' "$json_file")
+
+    # Check if a prompt exists
+    local prompt=$(jq -r --arg id "$id" '
+      .menu[] | 
+      .. | 
+      objects | 
+      select(.id == $id) | 
+      .prompt?' "$json_file")
+
+    # If a prompt exists, display it and wait for user confirmation
+    if [[ "$prompt" != "null" && $INPUTMODE != "cli" ]]; then
+        get_user_continue "$prompt" process_input
+    fi
+
+    # Execute each command
+    for command in "${commands[@]}"; do
+        [[ -n "$debug" ]] && echo "$command"
+        eval "$command"
+    done
+}
+
+
+
+
+
+function execute_command_old() {
     local id=$1
     #local commands=$(jq -r --arg id "$id" '.menu[] | .. | objects | select(.id==$id) | .command[]' "$json_file")
     local commands=$(jq -r --arg id "$id" '
