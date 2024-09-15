4fea75707481d87ed224e7532f5d6bd358934e86
Tearran <tearran@armbian.com>
2024-09-15 18:05:10 -0500

    added last login toggle

--- lib/armbian-configng/config.ng.system.sh
+++ lib/armbian-configng/config.ng.system.sh
@@ -176,3 +176,33 @@ fi
 # reboot is mandatory
 reboot
 }
+
+
+module_options+=(
+["toggle_ssh_lastlog,author"]="tearran"
+["toggle_ssh_lastlog,ref_link"]=""
+["toggle_ssh_lastlog,feature"]="toggle_ssh_lastlog"
+["toggle_ssh_lastlog,desc"]="Toggle SSH lastlog"
+["toggle_ssh_lastlog,example"]="toggle_ssh_lastlog"
+["toggle_ssh_lastlog,status"]="Active"
+)
+#
+# @description Toggle SSH lastlog
+#
+function toggle_ssh_lastlog(){
+
+    if ! grep -q '^#\?PrintLastLog ' "${SDCARD}/etc/ssh/sshd_config"; then
+    # If PrintLastLog is not found, append it with the value 'yes'
+    echo 'PrintLastLog no' >> "${SDCARD}/etc/ssh/sshd_config"
+    sudo service ssh restart
+else
+    # If PrintLastLog is found, toggle between 'yes' and 'no'
+    sed -i '/^#\?PrintLastLog /{
+        s/PrintLastLog yes/PrintLastLog no/;
+        t;
+        s/PrintLastLog no/PrintLastLog yes/
+    }' "${SDCARD}/etc/ssh/sshd_config"
+    sudo service ssh restart
+fi
+
+}
