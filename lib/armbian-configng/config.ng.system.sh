e8b4012c8360a9ee00bbbb079c2351be90ffc566
Gunjan Gupta <gunjan@wesion.com>
2024-09-16 22:05:32 +0530

    Add dtoverlay management
    Signed-off-by: Gunjan Gupta <gunjan@wesion.com>

--- lib/armbian-configng/config.ng.system.sh
+++ lib/armbian-configng/config.ng.system.sh
@@ -334,3 +334,55 @@ function adjust_motd() {
 	# adjust motd config
 	sed -i "s/^MOTD_DISABLE=.*/MOTD_DISABLE=\"$INSERT\"/g" /etc/default/armbian-motd
 }
+
+module_options+=(
+["manage_dtoverlays,author"]="Gunjan Gupta"
+["manage_dtoverlays,ref_link"]=""
+["manage_dtoverlays,feature"]="dtoverlays"
+["manage_dtoverlays,desc"]="Enable/disable device tree overlays"
+["manage_dtoverlays,example"]="manage_dtoverlays"
+["manage_dtoverlays,status"]="Active"
+)
+#
+# @description Enable/disable device tree overlays
+#
+function manage_dtoverlays () {
+	# check if user agree to enter this area
+	local changes="false"
+	local overlayconf="/boot/armbianEnv.txt"
+	while true; do
+		local options=()
+		j=0
+		available_overlays=$(ls -1 ${OVERLAYDIR}/*.dtbo | sed "s#^${OVERLAYDIR}/##" | sed 's/.dtbo//g' | grep $BOOT_SOC | tr '\n' ' ')
+		for overlay in ${available_overlays}; do
+			local status="OFF"
+			grep '^fdt_overlays' ${overlayconf} | grep -qw ${overlay} && status=ON
+			options+=( "$overlay" "" "$status")
+		done
+		selection=$(whiptail --title "Manage devicetree overlays" --cancel-button "Back" \
+			--ok-button "Save" --checklist "\nUse <space> to toggle functions and save them.\nExit when you are done.\n " \
+			0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
+		exit_status=$?
+		case $exit_status in
+			0)
+				changes="true"
+				newoverlays=$(echo $selection | sed 's/"//g')
+				sed -i "s/^fdt_overlays=.*/fdt_overlays=$newoverlays/" ${overlayconf}
+				if ! grep -q "^fdt_overlays" ${overlayconf}; then echo "fdt_overlays=$newoverlays" >> ${overlayconf}; fi
+				sync
+				;;
+			1)
+				if [[ "$changes" == "true" ]]; then
+					whiptail --title " Reboot required " --yes-button "Reboot" \
+						--no-button "Cancel" --yesno "A reboot is required to apply the changes. Shall we reboot now?" 7 34
+					if [[ $? = 0 ]]; then
+						reboot
+					fi
+				fi
+				break
+				;;
+			255)
+				;;
+		esac
+	done
+}
