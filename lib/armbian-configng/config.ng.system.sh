9b3bd6e2de5867eae824adbe69967bd150ee25c9
Igor <igor.pecovnik@gmail.com>
2024-09-23 20:46:34 +0200

    Add mechanism for motd management (#89)
    Co-authored-by: Joey Turner <Tearran@users.noreply.github.com>

--- lib/armbian-configng/config.ng.system.sh
+++ lib/armbian-configng/config.ng.system.sh
@@ -225,4 +225,65 @@ else
     sudo service ssh restart
 fi
 }
+
+module_options+=(
+["adjust_motd,author"]="igorpecovnik"
+["adjust_motd,ref_link"]=""
+["adjust_motd,feature"]="Adjust motd"
+["adjust_motd,desc"]="Adjust welcome screen (motd)"
+["adjust_motd,example"]=""
+["adjust_motd,status"]="Active"
+)
+#
+# @description Toggle message of the day items
+#
+function adjust_motd
+{
+
+	# show motd description
+	motd_desc(){
+		case $1 in
+            clear)
+                echo "Clear screen on login"
+            ;;
+            header)
+                echo "Show header with logo"
+            ;;
+            sysinfo)
+                echo "Display system information"
+            ;;
+            tips)
+                echo "Show Armbian team tips"
+            ;;
+            commands)
+                echo "Show recommended commands"
+            ;;
+            *)
+                echo "No description"
+            ;;
+        esac
+    }
+
+	# read status
+	function motd_status
+	{
+	source /etc/default/armbian-motd
+	if [[ $MOTD_DISABLE == *$1* ]] ; then
+		echo "OFF"
+	else
+		echo "ON"
+	fi
+	}
+
+	LIST=()
+	for v in $(grep THIS_SCRIPT= /etc/update-motd.d/* | cut -d"=" -f2 | sed "s/\"//g"); do
+		LIST+=("$v" "$(motd_desc $v)" "$(motd_status $v)")
+	done
+
+	INLIST=($(grep THIS_SCRIPT= /etc/update-motd.d/* | cut -d"=" -f2 | sed "s/\"//g"))
+	CHOICES=$(whiptail --separate-output --nocancel --title "Adjust welcome screen" --checklist "" 11 50 5 "${LIST[@]}" 3>&1 1>&2 2>&3)
+	INSERT=$(echo ${INLIST[@]} ${CHOICES[@]} | tr ' ' '\n' | sort | uniq -u | tr '\n' ' ' | sed 's/ *$//')
+	# adjust motd config
+	sed -i "s/^MOTD_DISABLE=.*/MOTD_DISABLE=\"$INSERT\"/g" /etc/default/armbian-motd
+
 }
