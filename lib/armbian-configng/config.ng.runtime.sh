e1e929078ec8ad6720238bfccb6f0977886683b8
Igor Pecovnik <igor@armbian.com>
2024-09-24 12:29:13 +0200

    Apply shfmt -w

--- lib/armbian-configng/config.ng.runtime.sh
+++ lib/armbian-configng/config.ng.runtime.sh
@@ -16,39 +16,37 @@ locale_setting="$LANG"
 installed_software="$(see_current_apt)"
 held_packages=$(apt-mark showhold)
 
-
 module_options+=(
-    ["update_json_data,author"]="Joey Turner"
-    ["update_json_data,ref_link"]=""
-    ["update_json_data,feature"]="update_json_data"
-    ["update_json_data,desc"]="Update JSON data with system information"
-    ["update_json_data,example"]="update_json_data"
-    ["update_json_data,status"]="review"
-    ["update_json_data,doc_link"]=""
+	["update_json_data,author"]="Joey Turner"
+	["update_json_data,ref_link"]=""
+	["update_json_data,feature"]="update_json_data"
+	["update_json_data,desc"]="Update JSON data with system information"
+	["update_json_data,example"]="update_json_data"
+	["update_json_data,status"]="review"
+	["update_json_data,doc_link"]=""
 
 )
 #
 # Update JSON data with system information
 update_json_data() {
-    json_data=$(echo "$json_data" | jq --arg key "$1" --arg value "$2" \
-        '(.menu[] | select(.id == $key).description) += " (" + $value + ")"')
+	json_data=$(echo "$json_data" | jq --arg key "$1" --arg value "$2" \
+		'(.menu[] | select(.id == $key).description) += " (" + $value + ")"')
 }
 
-
 module_options+=(
-    ["update_submenu_data,author"]="Joey Turner"
-    ["update_submenu_data,ref_link"]=""
-    ["update_submenu_data,feature"]="update_submenu_data"
-    ["update_submenu_data,desc"]="Update submenu descriptions based on conditions"
-    ["update_submenu_data,example"]="update_submenu_data"
-    ["update_submenu_data,status"]="review"
-    ["update_submenu_data,doc_link"]=""
+	["update_submenu_data,author"]="Joey Turner"
+	["update_submenu_data,ref_link"]=""
+	["update_submenu_data,feature"]="update_submenu_data"
+	["update_submenu_data,desc"]="Update submenu descriptions based on conditions"
+	["update_submenu_data,example"]="update_submenu_data"
+	["update_submenu_data,status"]="review"
+	["update_submenu_data,doc_link"]=""
 )
 #
 # Update submenu descriptions based on conditions
 update_submenu_data() {
-    json_data=$(echo "$json_data" | jq --arg key "$1" --arg subkey "$2" --arg value "$3" \
-        '(.menu[] | select(.id==$key).sub[] | select(.id == $subkey).description) += " (" + $value + ")"')
+	json_data=$(echo "$json_data" | jq --arg key "$1" --arg subkey "$2" --arg value "$3" \
+		'(.menu[] | select(.id==$key).sub[] | select(.id == $subkey).description) += " (" + $value + ")"')
 }
 
 #
@@ -64,7 +62,7 @@ update_json_data "Software" "$installed_software"
 
 # Conditional submenu updates based on network type
 if [ "$network_adapter" = "IPv6" ]; then
-    update_submenu_data "Network" "N08" "IPV6"
+	update_submenu_data "Network" "N08" "IPV6"
 else
-    update_submenu_data "Network" "N08" "IPV4"
+	update_submenu_data "Network" "N08" "IPV4"
 fi
