b16db304d9eee4425755a612180eea53e36ac532
Tearran <tearran@armbian.com>
2024-09-24 14:42:06 -0500

    styles fix

--- lib/armbian-configng/config.ng.software.sh
+++ lib/armbian-configng/config.ng.software.sh
@@ -95,7 +95,7 @@ install_docker() {
 		# Add the repository to Apt sources:
 		echo \
 			"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$distro \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+			$(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
 			sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		apt_install_wrapper apt-get update
 
