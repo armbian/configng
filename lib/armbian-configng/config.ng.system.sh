bddaebd563264c283547fd8230be23d5fba7679a
Igor Pecovnik <igor@armbian.com>
2024-09-27 13:45:15 +0200

    Enable distro upgrades procedure

--- lib/armbian-configng/config.ng.system.sh
+++ lib/armbian-configng/config.ng.system.sh
@@ -13,10 +13,59 @@ module_options+=(
 #
 function apt_install_wrapper() {
 	if [ -t 0 ]; then
-		debconf-apt-progress -- "$@"
+		DEBIAN_FRONTEND=noninteractive debconf-apt-progress -- "$@"
 	else
 		# Terminal not defined - proceed without TUI
-		"$@"
+		DEBIAN_FRONTEND=noninteractive "$@"
+	fi
+}
+
+module_options+=(
+	["release_upgrade,author"]="Igor Pecovnik"
+	["release_upgrade,ref_link"]=""
+	["release_upgrade,feature"]="Upgrade upstream distribution release"
+	["release_upgrade,desc"]="Upgrade to next stable or rolling release"
+	["release_upgrade,example"]="release_upgrade stable verify"
+	["release_upgrade,status"]="Active"
+)
+#
+# Upgrade distribution
+#
+release_upgrade(){
+
+	local upgrade_type=$1
+	local verify=$2
+
+	local distroid=${DISTROID}
+
+	if [[ "${upgrade_type}" == stable ]]; then
+		local filter=$(grep "supported" /etc/armbian-distribution-status | cut -d"=" -f1)
+	elif [[ "${upgrade_type}" == rolling ]]; then
+		local filter=$(grep "eos\|csc" /etc/armbian-distribution-status | cut -d"=" -f1 | sed "s/sid/testing/g")
+	else
+		local filter=$(cat /etc/armbian-distribution-status | cut -d"=" -f1)
+	fi
+
+	local upgrade=$(for j in $filter; do
+		for i in $(grep "^${distroid}" /etc/armbian-distribution-status | cut -d";" -f2 | cut -d"=" -f2 | sed "s/,/ /g"); do
+			if [[ $i == $j ]]; then
+				echo $i
+			fi
+		done
+	done | tail -1)
+
+	if [[ -z "${upgrade}" ]]; then
+		return 1;
+	elif [[ -z "${verify}" ]]; then
+		[[ -f /etc/apt/sources.list.d/ubuntu.sources ]] && sed -i "s/$distroid/$upgrade/g" /etc/apt/sources.list.d/ubuntu.sources
+		[[ -f /etc/apt/sources.list.d/debian.sources ]] && sed -i "s/$distroid/$upgrade/g" /etc/apt/sources.list.d/debian.sources
+		[[ -f /etc/apt/sources.list ]] && sed -i "s/$distroid/$upgrade/g" /etc/apt/sources.list
+		[[ "${upgrade}" == "testing" ]] && upgrade="sid" # our repo and everything is tied to sid
+		[[ -f /etc/apt/sources.list.d/armbian.list ]] && sed -i "s/$distroid/$upgrade/g" /etc/apt/sources.list.d/armbian.list
+		apt_install_wrapper apt-get -y update
+		apt_install_wrapper apt-get -y -o Dpkg::Options::="--force-confold" upgrade --without-new-pkgs
+		apt_install_wrapper apt-get -y -o Dpkg::Options::="--force-confold" full-upgrade
+		apt_install_wrapper apt-get -y --purge autoremove
 	fi
 }
 
--- tests/README.md
+++ tests/README.md
@@ -4,4 +4,4 @@
 - ENABLED=false|true
 - PREINSTALL="cmd" in order to satisfy test case
 - CONDITION must return 0 for test success
-
+- RELEASE="bookworm:jammy:noble" run on specific or leave empty to run on all
