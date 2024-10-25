2c57bebbfcd6ca74383acae859cd8bdd6bbcad1f
Tearran <tearran@gmail.com>
2024-10-25 10:52:27 -0700

    migrated and move network related to network

--- lib/armbian-config/config.functions.sh
+++ lib/armbian-config/config.functions.sh
@@ -1,35 +1,5 @@
 
 module_options+=(
-	["qr_code,author"]="Igor Pecovnik"
-	["qr_code,ref_link"]=""
-	["qr_code,feature"]="qr_code"
-	["qr_code,desc"]="Show or generate QR code for Google OTP"
-	["qr_code,example"]="qr_code generate"
-	["qr_code,status"]="Active"
-)
-#
-# check dpkg status of $1 -- currently only 'not installed at all' case caught
-#
-function qr_code() {
-
-	clear
-	if [[ "$1" == "generate" ]]; then
-		google-authenticator -t -d -f -r 3 -R 30 -W -q
-		cp /root/.google_authenticator /etc/skel
-		update_skel
-	fi
-	export TOP_SECRET=$(head -1 /root/.google_authenticator)
-	qrencode -m 2 -d 9 -8 -t ANSI256 "otpauth://totp/test?secret=$TOP_SECRET"
-	echo -e '
-Scan QR code with your OTP application on mobile phone
-'
-	read -n 1 -s -r -p "Press any key to continue"
-
-}
-
-
-
-module_options+=(
 	["check_desktop,author"]="Igor Pecovnik"
 	["check_desktop,ref_link"]=""
 	["check_desktop,feature"]="check_desktop"
@@ -547,76 +517,6 @@ sanitize_input() {
 		fi
 	done
 	echo "${sanitized_input[@]}"
-}
-
-module_options+=(
-	["connect_bt_interface,author"]="Igor Pecovnik"
-	["connect_bt_interface,ref_link"]=""
-	["connect_bt_interface,feature"]="connect_bt_interface"
-	["connect_bt_interface,desc"]="Migrated procedures from Armbian config."
-	["connect_bt_interface,example"]="connect_bt_interface"
-	["connect_bt_interface,status"]="Active"
-)
-#
-# connect to bluetooth device
-#
-function connect_bt_interface() {
-
-	IFS=$'
-'
-	GLOBIGNORE='*'
-	show_infobox <<< "
-Discovering Bluetooth devices ... "
-	BT_INTERFACES=($(hcitool scan | sed '1d'))
-
-	local LIST=()
-	for i in "${BT_INTERFACES[@]}"; do
-		local a=$(echo ${i[0]//[[:blank:]]/} | sed -e 's/^\(.\{17\}\).*/\1/')
-		local b=${i[0]//$a/}
-		local b=$(echo $b | sed -e 's/^[ 	]*//')
-		LIST+=("$a" "$b")
-	done
-
-	LIST_LENGTH=$((${#LIST[@]} / 2))
-	if [ "$LIST_LENGTH" -eq 0 ]; then
-		BT_ADAPTER=${WLAN_INTERFACES[0]}
-		show_message <<< "
-No nearby Bluetooth devices were found!"
-	else
-		exec 3>&1
-		BT_ADAPTER=$(whiptail --title "Select interface" \
-			--clear --menu "" $((6 + ${LIST_LENGTH})) 50 $LIST_LENGTH "${LIST[@]}" 2>&1 1>&3)
-		exec 3>&-
-		if [[ $BT_ADAPTER != "" ]]; then
-			show_infobox <<< "
-Connecting to $BT_ADAPTER "
-			BT_EXEC=$(
-				expect -c 'set prompt "#";set address '$BT_ADAPTER';spawn bluetoothctl;expect -re $prompt;send "disconnect $address
-";
-			sleep 1;send "remove $address
-";sleep 1;expect -re $prompt;send "scan on
-";sleep 8;send "scan off
-";
-			expect "Controller";send "trust $address
-";sleep 2;send "pair $address
-";sleep 2;send "connect $address
-";
-			send_user "
-Should be paired now.
-";sleep 2;send "quit
-";expect eof'
-			)
-			echo "$BT_EXEC" > /tmp/bt-connect-debug.log
-			if [[ $(echo "$BT_EXEC" | grep "Connection successful") != "" ]]; then
-				show_message <<< "
-Your device is ready to use!"
-			else
-				show_message <<< "
-Error connecting. Try again!"
-			fi
-		fi
-	fi
-
 }
 
 
---
+++ tools/modules/network/see_ping.sh
@@ -0,0 +1,34 @@
+
+module_options+=(
+	["see_ping,author"]="Joey Turner"
+	["see_ping,ref_link"]="https://github.com/Tearran/configng/blob/main/config.ng.functions.sh#632"
+	["see_ping,feature"]="see_ping"
+	["see_ping,desc"]="Check the internet connection with fallback DNS"
+	["see_ping,example"]="see_ping"
+	["see_ping,doc_link"]=""
+	["see_ping,status"]="review"
+)
+#
+# Function to check the internet connection
+#
+function see_ping() {
+	# List of servers to ping
+	servers=("1.1.1.1" "8.8.8.8")
+
+	# Check for internet connection
+	for server in "${servers[@]}"; do
+		if ping -q -c 1 -W 1 $server > /dev/null; then
+			echo "Internet connection: Present"
+			break
+		else
+			echo "Internet connection: Failed"
+			sleep 1
+		fi
+	done
+
+	if [[ $? -ne 0 ]]; then
+		read -n -r 1 -s -p "Warning: Configuration cannot work properly without a working internet connection. \
+		Press CTRL C to stop or any key to ignore and continue."
+	fi
+
+}
