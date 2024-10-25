61d84d2e499316e0b162b611e89c8d04bb293076
Igor Pecovnik <igor@armbian.com>
2024-10-25 08:03:53 +0200

    Example of adding new software title

--- lib/armbian-configng/config.ng.software.sh
+++ lib/armbian-configng/config.ng.software.sh
@@ -107,4 +107,40 @@ install_docker() {
 	fi
 }
 
+module_options+=(
+	["update_skel,author"]="Igor Pecovnik"
+	["update_skel,ref_link"]=""
+	["update_skel,feature"]="module_portainer"
+	["update_skel,desc"]="Install/uninstall/check status of portainer container"
+	["update_skel,example"]="module_portainer install|uninstall|status"
+	["update_skel,status"]="Active"
+)
+#
+# Install portainer container
+#
+module_portainer() {
 
+	if check_if_installed docker-ce; then
+		local container=$(docker container ls -a | mawk '/portainer\/portainer(-ce)?( |$)/{print $1;exit}')
+		local image=$(docker image ls -a | mawk '/portainer\/portainer(-ce)?( |$)/{print $3;exit}')
+	fi
+
+	case "$1" in
+		install)
+			check_if_installed docker-ce || install_docker
+			docker volume ls -q | grep -xq 'portainer_data' || docker volume create portainer_data
+			docker run -d -p '9002:9000' --name=portainer --restart=always \
+			-v '/run/docker.sock:/var/run/docker.sock' \
+			-v '/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt:ro' \
+			-v 'portainer_data:/data' 'portainer/portainer-ce'
+		;;
+		uninstall)
+			[[ "${container}" ]] && docker container rm -f "$container"
+			[[ "${image}" ]] && docker image rm "$image"
+		;;
+		status)
+			[[ "${container}" ]] || [[ "${image}" ]] && return 0
+		;;
+	esac
+
+}
