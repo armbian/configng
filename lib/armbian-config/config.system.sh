f0874d202d44f8274d99e4094e68578dee5f0d4c
Tearran <tearran@gmail.com>
2024-10-25 00:52:34 -0700

    move more

--- lib/armbian-config/config.system.sh
+++ lib/armbian-config/config.system.sh
@@ -1,4 +1,28 @@
 module_options+=(
+	["update_skel,author"]="Igor Pecovnik"
+	["update_skel,ref_link"]=""
+	["update_skel,feature"]="update_skel"
+	["update_skel,desc"]="Update the /etc/skel files in users directories"
+	["update_skel,example"]="update_skel"
+	["update_skel,status"]="Active"
+)
+#
+# check dpkg status of $1 -- currently only 'not installed at all' case caught
+#
+function update_skel() {
+
+	getent passwd |
+		while IFS=: read -r username x uid gid gecos home shell; do
+			if [ ! -d "$home" ] || [ "$username" == 'root' ] || [ "$uid" -lt 1000 ]; then
+				continue
+			fi
+			tar -C /etc/skel/ -cf - . | su - "$username" -c "tar --skip-old-files -xf -"
+		done
+
+}
+
+
+module_options+=(
 	["are_headers_installed,author"]="Gunjan Gupta"
 	["are_headers_installed,ref_link"]=""
 	["are_headers_installed,feature"]="are_headers_installed"
@@ -77,6 +101,83 @@ function set_header_remove() {
 }
 
 
+module_options+=(
+	["armbian_fw_manipulate,author"]="Igor Pecovnik"
+	["armbian_fw_manipulate,ref_link"]=""
+	["armbian_fw_manipulate,feature"]="armbian_fw_manipulate"
+	["armbian_fw_manipulate,desc"]="freeze/unhold/reinstall armbian related packages."
+	["armbian_fw_manipulate,example"]="armbian_fw_manipulate unhold|freeze|reinstall"
+	["armbian_fw_manipulate,status"]="Active"
+)
+#
+# freeze/unhold/reinstall armbian firmware packages
+#
+armbian_fw_manipulate() {
+
+	local function=$1
+	local version=$2
+	local branch=$3
+
+	[[ -n $version ]] && local version="=${version}"
+
+	# fallback to $BRANCH
+	[[ -z "${branch}" ]] && branch="${BRANCH}"
+	[[ -z "${branch}" ]] && branch="current" # fallback in case we switch to very old BSP that have no such info
+
+	# packages to install
+	local armbian_packages=(
+		"linux-u-boot-${BOARD}-${branch}"
+		"linux-image-${branch}-${LINUXFAMILY}"
+		"linux-dtb-${branch}-${LINUXFAMILY}"
+		"armbian-zsh"
+		"armbian-bsp-cli-${BOARD}-${branch}"
+	)
+
+	# reinstall headers only if they were previously installed
+	if are_headers_installed; then
+		local armbian_packages+="linux-headers-${branch}-${LINUXFAMILY}"
+	fi
+
+	local packages=""
+	for pkg in "${armbian_packages[@]}"; do
+		if [[ "${function}" == reinstall ]]; then
+			local pkg_search=$(apt search "$pkg" 2> /dev/null | grep "^$pkg")
+			if [[ $? -eq 0 && -n "${pkg_search}" ]]; then
+				if [[ "${pkg_search}" == *$version* ]] ; then
+				packages+="$pkg${version} ";
+				else
+				packages+="$pkg ";
+				fi
+			fi
+		else
+			if check_if_installed $pkg; then
+				packages+="$pkg${version} "
+			fi
+		fi
+	done
+	for pkg in "${packages[@]}"; do
+		case $function in
+			unhold) apt-mark unhold ${pkg} | show_infobox ;;
+			hold) apt-mark hold ${pkg} | show_infobox ;;
+			reinstall)
+				apt_install_wrapper apt-get -y --simulate --download-only --allow-change-held-packages --allow-downgrades install ${pkg}
+				if [[ $? == 0 ]]; then
+					apt_install_wrapper apt-get -y purge "linux-u-boot-*" "linux-image-*" "linux-dtb-*" "linux-headers-*" "armbian-zsh-*" "armbian-bsp-cli-*" # remove all branches
+					apt_install_wrapper apt-get -y --allow-change-held-packages install ${pkg}
+					apt_install_wrapper apt-get -y autoremove
+					apt_install_wrapper apt-get -y clean
+				else
+					exit 1
+				fi
+
+
+				;;
+			*) return ;;
+		esac
+	done
+}
+
+
 
 menu_options+=(
 	["get_headers_kernel,author"]="Igor Pecovnik"
@@ -125,6 +226,48 @@ function Headers_remove() {
 		# cleanup
 		apt clean
 		debconf-apt-progress -- apt -y autoremove
+	fi
+}
+
+
+module_options+=(
+	["switch_kernels,author"]="Igor"
+	["switch_kernels,ref_link"]=""
+	["switch_kernels,feature"]=""
+	["switch_kernels,desc"]="Switching to alternative kernels"
+	["switch_kernels,example"]=""
+	["switch_kernels,status"]="Active"
+)
+#
+# @description Switch between alternative kernels
+#
+function switch_kernels() {
+
+	# we only allow switching kerneles that are in the test pool
+	[[ -z "${KERNEL_TEST_TARGET}" ]] && KERNEL_TEST_TARGET="legacy,current,edge"
+	local kernel_test_target=$(for x in ${KERNEL_TEST_TARGET//,/ }; do echo "linux-image-$x-${LINUXFAMILY}"; done;)
+	local installed_kernel_version=$(dpkg -l | grep '^ii' | grep linux-image | awk '{print $2"="$3}')
+	# just in case current is not installed
+	[[ -n ${installed_kernel_version} ]] && local grep_current_kernel=" | grep -v ${installed_kernel_version}"
+	local search_exec="apt-cache show ${kernel_test_target} | grep -E \"Package:|Version:|version:|family\" | grep -v \"Config-Version\" | sed -n -e 's/^.*: //p' | sed 's/\.$//g' | xargs -n3 -d'
+' | sed \"s/ /=/\" $grep_current_kernel"
+	IFS=$'
+'
+	local LIST=()
+	for line in $(eval ${search_exec}); do
+		LIST+=($(echo $line | awk -F ' ' '{print $1 "      "}') $(echo $line | awk -F ' ' '{print "v"$2}'))
+	done
+	unset IFS
+	local list_length=$((${#LIST[@]} / 2))
+	if [ "$list_length" -eq 0 ]; then
+		dialog --backtitle "$BACKTITLE" --title " Warning " --msgbox "
+No other kernels available!" 7 32
+	else
+		local target_version=$(whiptail --separate-output --title "Select kernel" --menu "ed" $((${list_length} + 7)) 80 $((${list_length})) "${LIST[@]}" 3>&1 1>&2 2>&3)
+		if [ $? -eq 0 ] && [ -n "${target_version}" ]; then
+			local branch=${target_version##*image-}
+			armbian_fw_manipulate "reinstall" "${target_version/*=/}" "${branch%%-*}"
+		fi
 	fi
 }
 
