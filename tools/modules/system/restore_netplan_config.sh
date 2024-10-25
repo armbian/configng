6077934c4ca9df990e812c1ac31621798cd98f98
Tearran <tearran@gmail.com>
2024-10-25 09:18:14 -0700

    Migrated system

---
+++ tools/modules/system/restore_netplan_config.sh
@@ -0,0 +1,23 @@
+
+module_options+=(
+["store_netplan_config,author"]="Igor Pecovnik"
+["store_netplan_config,ref_link"]=""
+["store_netplan_config,feature"]="Storing netplan config to tmp"
+["store_netplan_config,desc"]=""
+["store_netplan_config,example"]=""
+["store_netplan_config,status"]="Active"
+)
+#
+# @description Restoring Netplan configuration from temp folder
+#
+function restore_netplan_config() {
+
+	echo "Restoring NetPlan configs" | show_infobox
+	# just in case
+	if [[ -n ${restore_netplan_config_folder} ]]; then
+		rm -f /etc/netplan/*
+		rsync -ar ${restore_netplan_config_folder}/. /etc/netplan
+	fi
+
+}
+
