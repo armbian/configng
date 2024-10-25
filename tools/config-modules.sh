0f76292e1f7670718ff50980283ce1c8e6f3c346
Tearran <tearran@gmail.com>
2024-10-25 10:29:51 -0700

    remove output folder befor assembling , styles

--- tools/config-modules.sh
+++ tools/config-modules.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(dirname "$0")"
 SRC_DIR="$SCRIPT_DIR/modules"
 DEST_DIR="$SCRIPT_DIR/../lib/armbian-config"
 
-[[ -d "$DEST_DIR" ]] && rm -rf "$DEST_DIR"
+[[ -d "$DEST_DIR" ]] && rm -rf "$DEST_DIR" && echo "remove $DEST_DIR"
 
 # Create the destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
