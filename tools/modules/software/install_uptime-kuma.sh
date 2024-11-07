e41c822279325e1cb40fdb0a691041a0dd4576c4
Igor Pecovnik <igor@armbian.com>
2024-11-07 10:51:51 +0100

    Software title: add Uptime Kuma, self-hosted monitoring tool

---
+++ tools/modules/software/install_uptime-kuma.sh
@@ -0,0 +1,34 @@
+module_options+=(
+	["module_uptimekuma,author"]="@armbian"
+	["module_uptimekuma,ref_link"]=""
+	["module_uptimekuma,feature"]="module_uptimekuma"
+	["module_uptimekuma,desc"]="Install/uninstall/check status of uptime kuma container"
+	["module_uptimekuma,example"]="module_uptimekuma install|uninstall|status"
+	["module_uptimekuma,status"]="Active"
+)
+#
+# Install uptime kuma
+#
+module_uptimekuma() {
+
+	if check_if_installed docker-ce; then
+		local container=$(docker container ls -a | mawk '/uptime-kuma?( |$)/{print $1}')
+		local image=$(docker image ls -a | mawk '/uptime-kuma?( |$)/{print $3}')
+	fi
+
+	case "$1" in
+		install)
+			check_if_installed docker-ce || install_docker
+			#docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:1 >/dev/null
+			docker run -d --quiet --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:1 >/dev/null
+			$DIALOG --msgbox "Uptime Kuma service has been installed successfully!\n\nIn a web browser go to http://localhost:3001 or \nhttp://127.0.0.1:3001 on this computer." 10 70
+		;;
+		uninstall)
+			[[ "${container}" ]] && docker container rm -f "$container" >/dev/null
+			[[ "${image}" ]] && docker image rm "$image" >/dev/null
+		;;
+		status)
+			[[ "${container}" ]] || [[ "${image}" ]] && return 0
+		;;
+	esac
+}
