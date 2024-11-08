3bc6f39d537a9deb3e8d860bd88ea1ca2c3db952
Tearran <tearran@gmail.com>
2024-11-07 19:18:05 -0700

    fix style

--- tools/template/module_software_template.sh
+++ tools/template/module_software_template.sh
@@ -10,35 +10,35 @@ module_options+=(
 )
 
 function module_template() {
-    local title="test"
-    local condition=$(which "$title" 2>/dev/null)
+	local title="test"
+	local condition=$(which "$title" 2>/dev/null)
 
-    # Convert the example string to an array
-    local commands
-    IFS=' ' read -r -a commands <<< "${module_options["module_template,example"]}"
+	# Convert the example string to an array
+	local commands
+	IFS=' ' read -r -a commands <<< "${module_options["module_template,example"]}"
 
-    case "$1" in
-        "${commands[0]}")
-            echo "Installing $title..."
-            # Installation logic here
-            ;;
-        "${commands[1]}")
-            echo "Removing $title..."
-            # Removal logic here
-            ;;
-        "${commands[2]}")
-		echo -e "\nUsage: ${module_options["module_template,feature"]} <command>"
-		echo -e "Commands:  ${module_options["module_template,example"]}"
-		echo "Available commands:"
-		echo -e "\tinstall\t- Install $title."
-		echo -e "\tremove\t- Remove $title."
-		echo
-            ;;
-        *)
-            ${module_options["module_template,feature"]} ${commands[2]}
-            ;;
-    esac
-}
+	case "$1" in
+		"${commands[0]}")
+		echo "Installing $title..."
+		# Installation logic here
+		;;
+		"${commands[1]}")
+		echo "Removing $title..."
+		# Removal logic here
+		;;
+		"${commands[2]}")
+			echo -e "\nUsage: ${module_options["module_template,feature"]} <command>"
+			echo -e "Commands:  ${module_options["module_template,example"]}"
+			echo "Available commands:"
+			echo -e "\tinstall\t- Install $title."
+			echo -e "\tremove\t- Remove $title."
+			echo
+		;;
+		*)
+		${module_options["module_template,feature"]} ${commands[2]}
+		;;
+	esac
+	}
 
 # uncomment to test the module
 module_template "$1"
