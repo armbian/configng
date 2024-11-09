0898102f304f76483e1afa26488b62d411beaa57
Tearran <tearran@gmail.com>
2024-11-08 19:34:07 -0700

    added help message

--- tools/modules/software/install_portainer.sh
+++ tools/modules/software/install_portainer.sh
@@ -1,9 +1,10 @@
+declare -A module_options
 module_options+=(
 	["module_portainer,author"]="@armbian"
 	["module_portainer,ref_link"]=""
 	["module_portainer,feature"]="module_portainer"
 	["module_portainer,desc"]="Install/uninstall/check status of portainer container"
-	["module_portainer,example"]="module_portainer install|uninstall|status"
+	["module_portainer,example"]="help install uninstall status"
 	["module_portainer,status"]="Active"
 )
 #
@@ -15,8 +16,24 @@ module_portainer() {
 		local container=$(docker container ls -a | mawk '/portainer\/portainer(-ce)?( |$)/{print $1}')
 		local image=$(docker image ls -a | mawk '/portainer\/portainer(-ce)?( |$)/{print $3}')
 	fi
+	# Convert the example string to an array
+	local commands
+	IFS=' ' read -r -a commands <<< "${module_options["module_portainer,example"]}"
 
 	case "$1" in
+		"${commands[0]}")
+			## help/menu options for the module
+			echo -e "\nUsage: ${module_options["module_webmin,feature"]} <command>"
+			echo -e "Commands: ${module_options["module_webmin,example"]}"
+			echo "Available commands:"
+			if [[ "${container}" ]] || [[ "${image}" ]]; then
+				echo -e "\tstatus\t- Show the status of the $title service."
+				echo -e "\tremove\t- Remove $title."
+			else
+				echo -e "  install\t- Install $title."
+			fi
+			echo
+		;;
 		install)
 			check_if_installed docker-ce || install_docker
 			docker volume ls -q | grep -xq 'portainer_data' || docker volume create portainer_data
@@ -34,3 +51,5 @@ module_portainer() {
 		;;
 	esac
 }
+
+#module_portainer help
