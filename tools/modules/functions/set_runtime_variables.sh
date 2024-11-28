83b48155ef289ea89f838a542c2e90d24535adda
Joey Turner <Tearran@users.noreply.github.com>
2024-11-28 02:20:23 -0600

    Update set_runtime_variables.sh

--- tools/modules/functions/set_runtime_variables.sh
+++ tools/modules/functions/set_runtime_variables.sh
@@ -65,3 +65,53 @@ function set_runtime_variables() {
 
 }
 
+
+module_options+=(
+	["update_json_data,author"]="@Tearran"
+	["update_json_data,ref_link"]=""
+	["update_json_data,feature"]="update_json_data"
+	["update_json_data,desc"]="Update JSON data with system information"
+	["update_json_data,example"]="update_json_data"
+	["update_json_data,status"]="review"
+	["update_json_data,doc_link"]=""
+
+)
+#
+# Update JSON data with system information
+update_json_data() {
+	json_data=$(echo "$json_data" | jq --arg key "$1" --arg value "$2" \
+		'(.menu[] | select(.id == $key).description) += " (" + $value + ")"')
+}
+
+module_options+=(
+	["update_submenu_data,author"]="@Tearran"
+	["update_submenu_data,ref_link"]=""
+	["update_submenu_data,feature"]="update_submenu_data"
+	["update_submenu_data,desc"]="Update submenu descriptions based on conditions"
+	["update_submenu_data,example"]="update_submenu_data"
+	["update_submenu_data,status"]="review"
+	["update_submenu_data,doc_link"]=""
+)
+#
+# Update submenu descriptions based on conditions
+update_submenu_data() {
+	json_data=$(echo "$json_data" | jq --arg key "$1" --arg subkey "$2" --arg value "$3" \
+		'(.menu[] | select(.id==$key).sub[] | select(.id == $subkey).description) += " (" + $value + ")"')
+}
+
+
+module_options+=(
+	["update_sub_submenu_data,author"]="@Tearran"
+	["update_sub_submenu_data,feature"]="update_sub_submenu_data"
+	["update_sub_submenu_data,desc"]="Update sub-submenu descriptions based on conditions"
+	["update_sub_submenu_data,example"]="update_sub_submenu_data \"MenuID\" \"SubID\" \"SubSubID\" \"CMD\""
+	["update_sub_submenu_data,status"]=""
+)
+#
+# Update sub-submenu descriptions based on conditions
+update_sub_submenu_data() {
+	json_data=$(echo "$json_data" | jq --arg key "$1" --arg subkey "$2" --arg subsubkey "$3" --arg value "$4" \
+		'(.menu[] | select(.id == $key).sub[] |
+		select(.id == $subkey).sub[] |
+		select(.id == $subsubkey).description) += " (" + $value + ")"')
+}
