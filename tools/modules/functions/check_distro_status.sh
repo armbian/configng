7dfbe3530c802048658e0639aae093d2c3380c63
Tearran <tearran@gmail.com>
2024-11-29 18:44:07 +0000

    replaced CHECK_OS with DISTRO_STATUS, fixed DISTROID, DISTRO, cleand up.

--- tools/modules/functions/check_distro_status.sh
+++ tools/modules/functions/check_distro_status.sh
@@ -7,7 +7,7 @@ module_options+=(
 	["check_os_status,status"]="Active"
 )
 
-function check_os_status() {
+function check_distro_status() {
 	case "$1" in
 		help)
 			echo "Usage: check_os_status"
@@ -15,26 +15,22 @@ function check_os_status() {
 			echo "It retrieves the current OS distribution and checks if it is listed as supported in the specified file."
 		;;
 		*)
-			FILE="/etc/armbian-distribution-status"
-
-			# Detect the current OS distribution codename
-			CURRENT_OS=$(lsb_release -cs 2>/dev/null || grep "VERSION=" /etc/os-release | grep -oP '(?<=\().*(?=\))')
 
 			# Ensure OS detection succeeded
-			if [[ -z "$CURRENT_OS" ]]; then
+			if [[ -z "$DISTROID" ]]; then
 			echo "Error: Unable to detect the current OS distribution."
 			exit 1
 			fi
 
-			# Check if the OS is listed as supported in the file
-			if grep -qE "^${CURRENT_OS}=.*supported" "$FILE"; then
-			echo "The current OS ($CURRENT_OS) is supported."
+			# Check if the OS is listed as supported in the DISTRO_STATUS
+			if grep -qE "^${DISTROID}=.*supported" "$DISTRO_STATUS"; then
+			echo "The current OS ($DISTROID) is supported."
 
 			else
-			BACKTITLE="Warning: The current OS ($CURRENT_OS) is not supported or not listed"
+			BACKTITLE="Warning: The current OS ($DISTROID) is not supported or not listed"
 			set_colors 1
 			get_user_continue "Warning:
-			The current OS ($CURRENT_OS) is not a supported distribution:
+			The current OS ($DISTROID) is not a supported distribution:
 			while the tool might still work well,
 			please be aware that issues may not be addressed by the maintainers.
 			You are welcome to contribute fixes for any problems you encounter.
