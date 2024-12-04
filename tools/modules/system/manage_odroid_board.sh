d04e3734556dd2741d3216b9e884e25bda8f1850
Igor Pecovnik <igor@armbian.com>
2024-12-04 23:49:02 +0100

    Menu adjustement and simplified condition, spaces to tabs, name with GH name

--- tools/modules/system/manage_odroid_board.sh
+++ tools/modules/system/manage_odroid_board.sh
@@ -1,42 +1,43 @@
 module_options+=(
-    ["manage_odroid_board,author"]="Geoff Clements"
-    ["manage_odroid_board,ref_link"]=""
-    ["manage_odroid_board,feature"]="Odroid board"
-    ["manage_odroid_board,desc"]="Select optimised Odroid board configuration"
-    ["manage_odroid_board,example"]="module_select_xuodroid"
-    ["manage_odroid_board,status"]="review"
+	["manage_odroid_board,author"]="@GeoffClements"
+	["manage_odroid_board,ref_link"]=""
+	["manage_odroid_board,feature"]="Odroid board"
+	["manage_odroid_board,desc"]="Select optimised Odroid board configuration"
+	["manage_odroid_board,example"]="select"
+	["manage_odroid_board,status"]="Stable"
 )
 #
 # @description Select optimised board configuration
 #
-function module_select_xuodroid() {
-    local board_list=("Odroid XU4" "Odroid XU3" "Odroid XU3 Lite" "Odroid HC1/HC2")
-    local board_id=("xu4" "xu3" "xu3l" "hc1")
-    local -a list
-    local state
+function manage_odroid_board() {
 
-    local env_file=/boot/armbianEnv.txt
-    local current_board=$(grep -oP '^board_name=\K.*' ${env_file})
-    local target_board=${current_board}
+	local board_list=("Odroid XU4" "Odroid XU3" "Odroid XU3 Lite" "Odroid HC1/HC2")
+	local board_id=("xu4" "xu3" "xu3l" "hc1")
+	local -a list
+	local state
 
-    for board_num in $(seq 0 $((${#board_list[@]} - 1))); do
-        if [[ "${board_id[${board_num}]}" == "${current_board}" ]]; then
-            state=on
-        else
-            state=off
-        fi
-        list+=("${board_id[${board_num}]}" "${board_list[${board_num}]}" "${state}")
-    done
+	local env_file=/boot/armbianEnv.txt
+	local current_board=$(grep -oP '^board_name=\K.*' ${env_file})
+	local target_board=${current_board}
 
-    target_board=$($DIALOG --notags --title "Select optimised board configuration" --radiolist "" 10 42 4 "${list[@]}" 3>&1 1>&2 2>&3)
-    if [[ $? == 0 ]]; then
-        sed -i "s/^board_name=.*/board_name=${target_board}/" ${env_file} 2> /dev/null && \
-            grep -q "^board_name=${target_board}" ${env_file} 2>/dev/null || \
-            echo "board_name=${target_board}" >> ${env_file}
-        sed -i "s/^BOARD_NAME.*/BOARD_NAME=\"Odroid ${target_board^^}\"/" /etc/armbian-release
-        
-        $DIALOG --title " Reboot required " --yes-button "Reboot" \
-            --no-button "Cancel" --yesno "A reboot is required to apply the changes. Shall we reboot now?" 7 34
-        [[ $? == 0 ]] && reboot
-    fi
+	for board_num in $(seq 0 $((${#board_list[@]} - 1))); do
+		if [[ "${board_id[${board_num}]}" == "${current_board}" ]]; then
+			state=on
+		else
+			state=off
+		fi
+	list+=("${board_id[${board_num}]}" "${board_list[${board_num}]}" "${state}")
+	done
+
+	target_board=$($DIALOG --notags --title "Select optimised board configuration" --radiolist "" 10 42 4 "${list[@]}" 3>&1 1>&2 2>&3)
+	if [[ $? == 0 ]]; then
+		sed -i "s/^board_name=.*/board_name=${target_board}/" ${env_file} 2> /dev/null && \
+		grep -q "^board_name=${target_board}" ${env_file} 2>/dev/null || \
+		echo "board_name=${target_board}" >> ${env_file}
+		sed -i "s/^BOARD_NAME.*/BOARD_NAME=\"Odroid ${target_board^^}\"/" /etc/armbian-release
+
+		$DIALOG --title " Reboot required " --yes-button "Reboot" \
+		--no-button "Cancel" --yesno "A reboot is required to apply the changes. Shall we reboot now?" 7 34
+		[[ $? == 0 ]] && reboot
+	fi
 }
