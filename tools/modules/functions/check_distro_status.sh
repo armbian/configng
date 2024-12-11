efd55960ab547465ba41cbe0f2a999b076db272e
Igor Pecovnik <igor@armbian.com>
2024-12-11 11:52:22 +0100

    Cosmetic: fix line breaking in warning message

--- tools/modules/functions/check_distro_status.sh
+++ tools/modules/functions/check_distro_status.sh
@@ -31,12 +31,11 @@ function check_distro_status() {
 			set_colors 1
 			get_user_continue "Warning:
 
-			The current OS ($DISTROID) is not a officially supported distribution!
+			The current OS ($DISTROID) is not a officially supported distro!
 
 			The tool might still work well, but be aware that issues may
-			not be accepted and addressed by the maintainers. However, you are
-			welcome to contribute fixes for any problems you encounter.
-
+			not be accepted and addressed by the maintainers. However, you
+			are welcome to contribute fixes for any problems you encounter.
 
 			Would you like to continue?
 			" process_input
