13726a5405f128b39972a115c205ce8c0493cfdb
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-12-13 15:28:31 -0500

    package: refactor & sort

--- tools/modules/functions/package.sh
+++ tools/modules/functions/package.sh
@@ -1,5 +1,8 @@
 # package.sh
 
+# internal function
+_pkg_have_stdin() { [[ -t 0 ]] }
+
 declare -A module_options
 module_options+=(
 	["package,author"]="@dimitry-ishenko"
@@ -11,24 +14,7 @@ module_options+=(
 
 pkg_install()
 {
-	local apt=(apt-get -y)
-	[[ -t 0 ]] && apt=(debconf-apt-progress -- ${apt[@]})
-	${apt[@]} install "$@"
-}
-
-module_options+=(
-	["package,author"]="@dimitry-ishenko"
-	["package,desc"]="Remove package"
-	["package,example"]="pkg_remove nmap"
-	["package,feature"]="pkg_remove"
-	["package,status"]="Interface"
-)
-
-pkg_remove()
-{
-	local apt=(apt-get -y)
-	[[ -t 0 ]] && apt=(debconf-apt-progress -- ${apt[@]})
-	${apt[@]} autopurge "$@"
+	_pkg_have_stdin && debconf-apt-progress -- apt-get -y install "$@" || apt-get -y install "$@"
 }
 
 module_options+=(
@@ -43,4 +29,17 @@ pkg_installed()
 {
 	local status=$(dpkg -s "$1" 2>/dev/null | sed -n "s/Status: //p")
 	! [[ -z "$status" || "$status" = *deinstall* || "$status" = *not-installed* ]]
+}
+
+module_options+=(
+	["package,author"]="@dimitry-ishenko"
+	["package,desc"]="Remove package"
+	["package,example"]="pkg_remove nmap"
+	["package,feature"]="pkg_remove"
+	["package,status"]="Interface"
+)
+
+pkg_remove()
+{
+	_pkg_have_stdin && debconf-apt-progress -- apt-get -y autopurge "$@" || apt-get -y autopurge "$@"
 }
