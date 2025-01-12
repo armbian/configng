c8126da236c1cc1909f54e0af33426953c23a372
Igor Pecovnik <igor@armbian.com>
2025-01-12 12:20:41 +0100

    Add bridges networking to all existing bridges in the system
    and cleanup.

--- tools/modules/software/module_cockpit.sh
+++ tools/modules/software/module_cockpit.sh
@@ -2,7 +2,7 @@ module_options+=(
 	["module_cockpit,author"]="@tearran"
 	["module_cockpit,maintainer"]="@igorpecovnik"
 	["module_cockpit,feature"]="module_cockpit"
-	["module_cockpit,example"]="help install remove start stop enable disable status check"
+	["module_cockpit,example"]="install remove status help"
 	["module_cockpit,desc"]="Cockpit setup and service setting."
 	["module_cockpit,status"]="Stable"
 	["module_cockpit,doc_link"]="https://cockpit-project.org/guide/latest/"
@@ -14,103 +14,74 @@ module_options+=(
 function module_cockpit() {
 	local title="cockpit"
 	local condition=$(dpkg -s "cockpit" 2>/dev/null | sed -n "s/Status: //p")
+
 	# Convert the example string to an array
 	local commands
 	IFS=' ' read -r -a commands <<< "${module_options["module_cockpit,example"]}"
 
 	case "$1" in
-		"${commands[0]}")
-		## help/menu options for the module
-		echo -e "\nUsage: ${module_options["module_cockpit,feature"]} <command>"
-		echo -e "Commands: ${module_options["module_cockpit,example"]}"
-		echo "Available commands:"
-		if [[ -z "$condition" ]]; then
-			echo -e "  install\t- Install $title."
-		else
-			if [[ "$(systemctl is-active cockpit.socket 2>/dev/null)" == "active" ]]; then
-				echo -e "\tstop\t- Stop the $title service."
-			else
-			echo -e "\tstart\t- Start the $title service."
-			fi
-			if [[ $(systemctl is-enabled cockpit.socket) == "enabled" ]]; then
-			echo -e "\tdisable\t- Disable $title from starting on boot."
-			elif [[ $(systemctl is-enabled cockpit.socket) == "disabled" ]]; then
-			echo -e "\tenable\t- Enable $title to start on boot."
 
+		"${commands[0]}")
+
+			## install cockpit
+			pkg_update
+			pkg_install cockpit cockpit-ws cockpit-system cockpit-storaged cockpit-machines dnsmasq virtinst qemu-kvm qemu-utils
+
+			usermod -a -G libvirt libvirtdbus
+			usermod -a -G libvirt libvirt-qemu
+
+			# add bridged networking if bridges exists on the system
+			for f in /sys/class/net/*; do
+				intf=$(basename $f)
+				if [[ $intf =~ ^br[0-9] ]]; then
+					cat <<- EOF > /etc/libvirt/kvm-hostbridge-${intf}.xml
+					<network>
+					<name>hostbridge-${intf}</name>
+					<forward mode="bridge"/>
+					<bridge name="${intf}"/>
+					</network>
+					EOF
+					virsh net-define /etc/libvirt/kvm-hostbridge-${intf}.xml
+					virsh net-start hostbridge-${intf}
+					virsh net-autostart hostbridge-${intf}
+				fi
+			done
+			if $DIALOG --title " Reboot required " --yes-button "Reboot" --no-button "Cancel" --yesno \
+				"A reboot is required to start $title properly. Shall we reboot now?" 7 34; then
+				reboot
 			fi
-			echo -e "\tstatus\t- Show the status of the $title service."
-			echo -e "\tremove\t- Remove $title."
-		fi
-		echo
+
 		;;
 		"${commands[1]}")
-		## install cockpit
-		pkg_update
-		pkg_install cockpit cockpit-ws cockpit-system cockpit-storaged cockpit-machines samba dnsmasq virtinst qemu-kvm qemu-utils
-		# add bridged networking
-		cat <<- EOF > /etc/libvirt/kvm-hostbridge.xml
-		<network>
-		<name>hostbridge</name>
-		<forward mode="bridge"/>
-		<bridge name="br0"/>
-		</network>
-		EOF
-		virsh net-define /etc/libvirt/kvm-hostbridge.xml
-		virsh net-start hostbridge
-		virsh net-autostart hostbridge
+			## remove cockpit
+			systemctl stop cockpit.socket 2>/dev/null
+			systemctl stop cockpit 2>/dev/null
+			systemctl disable cockpit 2>/dev/null
+			for bridge in $(grep hostbridge /etc/libvirt/kvm-hostbridge-br* | grep -o -P '(?<=name>).*(?=\</name)'); do
+				virsh net-destroy ${bridge}
+				virsh net-undefine ${bridge}
+			done
+			pkg_remove cockpit cockpit-ws cockpit-system cockpit-storaged cockpit-machines dnsmasq virtinst qemu-kvm qemu-utils
 
-		echo "Cockpit installed successfully."
 		;;
 		"${commands[2]}")
-		## remove cockpit
-		systemctl disable cockpit cockpit.socket
-		pkg_remove cockpit
-		echo "Cockpit removed successfully."
+			if pkg_installed cockpit; then
+				return 0
+			else
+				return 1
+			fi
 		;;
 		"${commands[3]}")
-		## start cockpit
-
-		systemctl start cockpit.socket
-		echo "Cockpit service started."
-		;;
-		"${commands[4]}")
-		## stop cockpit
-
-		systemctl stop cockpit.socket
-		echo "Cockpit service stopped."
-		;;
-		"${commands[5]}")
-		## enable cockpit
-		#systemctl enable cockpit
-		systemctl enable cockpit.socket
-		echo "Cockpit service enabled."
-		;;
-		"${commands[6]}")
-		## disable cockpit
-		#systemctl disable cockpit
-		systemctl disable cockpit.socket
-		echo "Cockpit service disabled."
-		;;
-		"${commands[7]}")
-		## status cockpit
-		#systemctl status cockpit
-		systemctl status cockpit.socket
-		;;
-		"${commands[-1]}")
-		## check cockpit status
-		if [[ $(systemctl is-active cockpit.socket) == "active" ]]; then
-			echo "Cockpit service is active."
-			return 0
-		elif [[ $(systemctl is-enabled cockpit.socket) == "disabled" ]]; then
-			echo "Cockpit service is disabled."
-			return 0
-		else
-			return 1
-		fi
+			echo -e "\nUsage: ${module_options["module_cockpit,feature"]} <command>"
+			echo -e "Commands:  ${module_options["module_cockpit,example"]}"
+			echo "Available commands:"
+			echo -e "\tinstall\t- Install $title."
+			echo -e "\tremove\t- Remove $title."
+			echo -e "\tstatus\t- Status $title."
+			echo
 		;;
 		*)
-		echo "Invalid command. Try: '${module_options["module_cockpit,example"]}'"
+			${module_options["module_cockpit,feature"]} ${commands[3]}
 		;;
 	esac
 }
-
