01d8d568ad3e308fa19db2f15b6d4346bf24f03e
Tearran <tearran@gmail.com>
2025-02-16 16:28:27 +0000

    added misson module_option for checkpoint debugging

--- tools/modules/initialize/message_checkpoint.sh
+++ tools/modules/initialize/message_checkpoint.sh
@@ -1,3 +1,4 @@
+
 module_options+=(
 	["set_checkpoint,author"]="@armbian"
 	["set_checkpoint,maintainer"]="@igorpecovnik"
@@ -10,23 +11,6 @@ module_options+=(
 	["set_checkpoint,port"]=""
 	["set_checkpoint,arch"]="x86-64 arm64"
 )
-
-module_options+=(
-	["debug_checkpoint,author"]="@armbian"
-	["debug_checkpoint,maintainer"]="@igorpecovnik"
-	["debug_checkpoint,feature"]="debug_checkpoint"
-	["debug_checkpoint,example"]="help start mark stop show"
-	["debug_checkpoint,desc"]="Helper module for debug info"
-	["debug_checkpoint,status"]=""
-	["debug_checkpoint,doc_link"]=""
-	["debug_checkpoint,group"]="Development"
-	["debug_checkpoint,port"]=""
-	["debug_checkpoint,arch"]="x86-64 arm64"
-)
-debug_checkpoint() {
-        checkpoint "$@"
-}
-
 #
 # Function to manage timer with multiple checkpoints
 function set_checkpoint() {
@@ -101,7 +85,7 @@ module_options+=(
 	["checkpoint,arch"]="x86-64 arm64"
 )
 # Example usage
-# set_checkpoint aluse with debugging
+# appended set_checkpoint with debugging
 checkpoint() {
 	case "$1" in
 		start|stop|mark|show)
