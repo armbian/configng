05eee9e01c1abf6ebab3e00807cfb70973f3ab70
Igor Pecovnik <igor@armbian.com>
2025-02-19 14:25:16 +0100

    Raspberry Pi: add support for managing overlays

--- tools/modules/system/manage_dtoverlays.sh
+++ tools/modules/system/manage_dtoverlays.sh
@@ -18,9 +18,10 @@ function manage_dtoverlays () {
 	# check if user agree to enter this area
 	local changes="false"
 	local overlayconf="/boot/armbianEnv.txt"
-	local overlaydir="$(find /boot/dtb/ -name overlay -and -type d)"
+	# Raspberry Pi has different name
+	local overlaydir="$(find /boot/dtb/ -type d \( -name overlay -o -name overlays \))"
 	local overlay_prefix=$(awk -F"=" '/overlay_prefix/ {print $2}' $overlayconf)
-	if [[ -z $(find ${overlaydir} -name *$overlay_prefix* 2> /dev/null) ]] ; then
+	if [[ -z $(find ${overlaydir} -name *$overlay_prefix* 2> /dev/null) && "${LINUXFAMILY}" != bcm2711 ]] ; then
 		echo "Invalid overlay_prefix $overlay_prefix"; exit 1
 	fi
 
@@ -30,14 +31,21 @@ function manage_dtoverlays () {
 		local options=()
 		j=0
 
-		if [[ -n "${BOOT_SOC}" ]]; then
+		# read overlays
+		if [[ "${LINUXFAMILY}" == bcm2711 ]]; then
+			available_overlays=$(ls -1 ${overlaydir}/*.dtbo | sed 's/.dtbo//g' | awk -F'/' '{print $NF}')
+			overlayconf="/boot/firmware/config.txt"
+		elif [[ -n "${BOOT_SOC}" ]]; then
 			available_overlays=$(ls -1 ${overlaydir}/${overlay_prefix}*.dtbo | sed 's/^.*\('${overlay_prefix}'.*\)/\1/g' | grep -E "$BOOT_SOC|$BOARD" | sed 's/'${overlay_prefix}'-//g' | sed 's/.dtbo//g')
 		else
 			available_overlays=$(ls -1 ${overlaydir}/${overlay_prefix}*.dtbo | sed 's/^.*\('${overlay_prefix}'.*\)/\1/g' | sed 's/'${overlay_prefix}'-//g' | sed 's/.dtbo//g')
 		fi
+
 		for overlay in ${available_overlays}; do
 			local status="OFF"
 			grep '^overlays' ${overlayconf} | grep -qw ${overlay} && status=ON
+			# Raspberry Pi
+			grep '^dtoverlay' ${overlayconf} | grep -qw ${overlay} && status=ON
 			options+=( "$overlay" "" "$status")
 		done
 		selection=$($DIALOG --title "Manage devicetree overlays" --cancel-button "Back" \
@@ -48,9 +56,24 @@ function manage_dtoverlays () {
 			0)
 				changes="true"
 				newoverlays=$(echo $selection | sed 's/"//g')
-				sed -i "s/^overlays=.*/overlays=$newoverlays/" ${overlayconf}
-				if ! grep -q "^overlays" ${overlayconf}; then echo "overlays=$newoverlays" >> ${overlayconf}; fi
-				sync
+				# Raspberry Pi
+				if [[ "${LINUXFAMILY}" == bcm2711 ]]; then
+					if ! grep -q "# Armbian config" "${overlayconf}"; then
+						# Using sed to append at the end
+						sed -i "\$a \
+						# Armbian config" "${overlayconf}"
+					fi
+					# Delete Armbian config section
+					sed -i '/# Armbian config/,/^$/ { /# Armbian config/!d }' "${overlayconf}"
+					for overlay in ${newoverlays}; do
+						if ! grep -q "^overlays" ${overlayconf}; then echo
+							sed -i "/# Armbian config/a dtoverlay=$overlay"	"${overlayconf}"
+						fi
+					done
+				else
+					sed -i "s/^overlays=.*/overlays=$newoverlays/" ${overlayconf}
+					if ! grep -q "^overlays" ${overlayconf}; then echo "overlays=$newoverlays" >> ${overlayconf}; fi
+				fi
 				;;
 			1)
 				if [[ "$changes" == "true" ]]; then
