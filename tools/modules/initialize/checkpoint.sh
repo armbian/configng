f407722dd738aed4d736d96063fadc386a788545
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-03-25 23:25:20 -0400

    checkpoint: info -> mark

--- tools/modules/initialize/checkpoint.sh
+++ tools/modules/initialize/checkpoint.sh
@@ -3,7 +3,7 @@
 module_options+=(
 	["checkpoint,author"]="@dimitry-ishenko"
 	["checkpoint,feature"]="checkpoint"
-	["checkpoint,example"]="debug help info reset total"
+	["checkpoint,example"]="debug help mark reset total"
 	["checkpoint,desc"]="Manage checkpoints"
 	["checkpoint,status"]="Active"
 	["checkpoint,group"]="Development"
@@ -18,7 +18,7 @@ _checkpoint_add()
 		printf "%-30s %4d sec\n" "$msg" $((time - _checkpoint_time))
 		_checkpoint_time=$time
 
-	elif [[ -n "$UXMODE" && "$type" == info ]]; then
+	elif [[ -n "$UXMODE" && "$type" == mark ]]; then
 		_checkpoint_time=$(date +%s)
 		echo "$msg"
 	fi
@@ -31,12 +31,12 @@ Usage: ${module_options[checkpoint,feature]} <action> <message>
 Where <action> is one of:
 	debug      Show message in debug mode (DEBUG non-zero).
 	help       Show this help screen.
-	info       Show message in UI or debug mode.
+	mark       Show message in UI or debug mode.
 	reset      (Re)set starting point.
 	total      Show total time and reset (in debug mode).
 
 The 'debug' command will show time elapsed since the previous checkpoint after
-the <message>. The 'info' command will also show the elapsed time if the debug
+the <message>. The 'mark' command will also show the elapsed time if the debug
 mode is active (the DEBUG env var is non-zero).
 "
 }
@@ -62,7 +62,7 @@ checkpoint()
 	case "$1" in
 		debug) _checkpoint_add "$1" "$2";;
 		help)  _checkpoint_help;;
-		info)  _checkpoint_add "$1" "$2";;
+		mark)  _checkpoint_add "$1" "$2";;
 		reset) _checkpoint_reset;;
 		total) _checkpoint_total;;
 	esac
