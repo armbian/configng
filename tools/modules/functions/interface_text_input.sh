dffce8219ab9a67f13a4b899835a234439e0502c
Tearran <tearran@gmail.com>
2025-04-12 16:23:16 +0000

    aded samba module and text input interface

---
+++ tools/modules/functions/interface_text_input.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+module_options+=(
+	["_prompt_text_input,author"]="@Tearran"
+	["_prompt_text_input,feature"]="prompt_text_input"
+	["_prompt_text_input,example"]=""
+	["_prompt_text_input,desc"]="Prompt the user for text input with a default value"
+	["_prompt_text_input,status"]="Draft"
+)
+
+# This function prompts the user for text input, with a default value provided.
+# It uses different dialog tools (`whiptail`, `dialog`) or plain `read` based on the environment.
+#
+# Draft Note:
+# The function may not but should handle empty results to avoid continuing with invalid input.
+function _prompt_text_input() {
+	local prompt_text="$1"
+	local default_value="$2"
+	local result
+
+	if [[ "$DIALOG" == "whiptail" ]]; then
+		result=$(whiptail --inputbox "$prompt_text" 8 39 "$default_value" 3>&1 1>&2 2>&3)
+	elif [[ "$DIALOG" == "dialog" ]]; then
+		result=$(dialog --inputbox "$prompt_text" 8 39 "$default_value" 3>&1 1>&2 2>&3)
+	else
+		read -p "$prompt_text [$default_value]: " result
+		result=${result:-$default_value}
+	fi
+
+	# Check if the user cancelled the input (result is empty)
+	# Consider logging the cancellation event for debugging purposes.
+	if [[ -z "$result" ]]; then
+		echo "Input cancelled by user."
+		return 1
+	fi
+
+	echo "$result"
+}
+
+
+################################################################
+# The following is whiptail and dialog useage examples
+# for selective tui text input
+################################################################
+
+# Main function for the module
+function _edit_playground() {
+	case "$1" in
+		"help")
+			# Help message formatted for see_menu
+			echo "
+Usage: _edit_playground [option]
+Options:
+	workgroup	- Change the workgroup name
+	server_string	- Update the server string
+	netbios_name	- Modify the NetBIOS name
+	share_path	- Adjust the path for the Samba share
+			"
+			;;
+		"workgroup")
+			workgroup=$(_prompt_text_input "Enter the workgroup" "WORKGROUP")
+			echo "$workgroup"
+			;;
+		"server_string")
+			server_string=$(_prompt_text_input "Enter the server string" "Samba Server %v")
+			echo "$server_string"
+			;;
+		"netbios_name")
+			netbios_name=$(_prompt_text_input "Enter the NetBIOS name" "ubuntu")
+			echo "$netbios_name"
+			;;
+		"share_path")
+			share_path=$(_prompt_text_input "Enter the path for the Samba share" "/srv/samba/anonymous")
+			echo "$share_path"
+			;;
+		*)
+			echo "Invalid option. Use 'help' for usage information."
+			;;
+	esac
+}
+
+# Use case examples for TUI with case switch
+function module_tui_playground() {
+	case "$1" in
+		"help")
+		echo "
+Usage: module_tui_playground [option]
+Options:
+	text_input   - multiple prompts in a row.
+	see_menu     - Prompt for selectable text inputs
+	dialog       - Same as see_menu and uses dialog if installed
+		"
+		;;
+		"text_input")
+		workgroup=$(_prompt_text_input "Enter the workgroup" "WORKGROUP")
+		server_string=$(_prompt_text_input "Enter the server string" "Samba Server %v")
+		netbios_name=$(_prompt_text_input "Enter the netbios name" "ubuntu")
+		share_path=$(_prompt_text_input "Enter the path for the Samba share" "/srv/samba/anonymous")
+		echo -e "Workgroup: $workgroup\nServer String: $server_string\nNetBIOS Name: $netbios_name\nShare Path: $share_path" | show_message
+		;;
+		"see_menu")
+			see_menu _edit_playground | show_message
+		;;
+		"dialog")
+		DIALOG="dialog"
+
+		echo -e "This is an Ok dialog that is using $DIALOG" | show_message
+		see_menu _edit_playground
+		echo "This is an info box using $DIALOG" | show_infobox
+		;;
+		*)
+		echo "Invalid option. Use 'help' for usage information."
+		;;
+	esac
+	}
+
+#######################################################
+# End example
+######################################################
---
+++ tools/modules/software/module_samba.sh
@@ -0,0 +1,133 @@
+module_options+=(
+	["module_samba,author"]="@Tearran"
+	["module_samba,maintainer"]="@Tearran"
+	["module_samba,feature"]="module_samba"
+	["module_samba,example"]="help install remove start stop enable disable configure"
+	["module_samba,desc"]="Samba setup and service setting."
+	["module_samba,status"]="Active"
+	["module_samba,doc_link"]="https://www.samba.org/samba/docs/"
+	["module_samba,group"]="Networking"
+	["module_samba,port"]="445"
+	["module_samba,arch"]="x86-64 arm64 armhf"
+)
+
+function module_samba() {
+	local title="samba"
+	local condition=$(which smbd)
+
+	# Set the interface for dialog tools
+	set_interface
+
+	# Convert the example string to an array
+	local commands
+	IFS=' ' read -r -a commands <<< "${module_options["module_samba,example"]}"
+
+	case "$1" in
+		"${commands[0]}"|"")
+		## help/menu options for the module
+		echo -e "\nUsage: ${module_options["module_samba,feature"]} <command>"
+		echo -e "Commands: ${module_options["module_samba,example"]}"
+		echo "Available commands:"
+		if [[ -z "$condition" ]]; then
+			echo -e "  install\t- Install $title."
+		else
+			if srv_active smbd; then
+			echo -e "\tstop\t- Stop the $title service."
+			echo -e "\tdisable\t- Disable $title from starting on boot."
+			else
+			echo -e "\tenable\t- Enable $title to start on boot."
+			echo -e "\tstart\t- Start the $title service."
+			fi
+			echo -e "\tremove\t- Remove $title."
+			echo -e "\tconfigure\t- Configure $title."
+		fi
+		echo
+		;;
+		"${commands[1]}")
+		## install samba
+		pkg_update
+		pkg_install samba
+
+		# Backup the original Samba configuration file
+		sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
+
+		echo "Samba installed successfully."
+		;;
+		"${commands[2]}")
+		## remove samba
+		srv_disable smbd
+		pkg_remove samba
+		rm /etc/samba/smb.conf
+		echo "Samba removed successfully."
+		;;
+		"${commands[3]}")
+		srv_start smbd
+		echo "Samba service started."
+		;;
+		"${commands[4]}")
+		srv_stop smbd
+		echo "Samba service stopped."
+		;;
+		"${commands[5]}")
+		srv_enable smbd
+		echo "Samba service enabled."
+		;;
+		"${commands[6]}")
+		srv_disable smbd
+		echo "Samba service disabled."
+		;;
+		"${commands[7]}")
+		## configure samba
+
+		workgroup=$(_prompt_text_input "Enter the workgroup" "WORKGROUP")
+		server_string=$(_prompt_text_input "Enter the server string" "Samba Server %v")
+		netbios_name=$(_prompt_text_input "Enter the netbios name" "armbian")
+		share_path=$(_prompt_text_input "Enter the path for the Samba share" "/srv/samba/anonymous")
+
+		# Create a new Samba configuration file
+		cat <<EOL > /etc/samba/smb.conf
+[global]
+	workgroup = $workgroup
+	server string = $server_string
+	netbios name = $netbios_name
+	security = user
+	map to guest = bad user
+	dns proxy = no
+
+[Anonymous]
+	path = $share_path
+	browseable = yes
+	writable = yes
+	guest ok = yes
+	read only = no
+EOL
+
+		# Create a directory for the Samba share
+		sudo mkdir -p $share_path
+		sudo chown -R nobody:nogroup $share_path
+		sudo chmod -R 0775 $share_path
+
+		# Restart Samba services
+		sudo systemctl restart smbd
+		sudo systemctl restart nmbd
+
+		echo "Samba configured successfully."
+		;;
+		"${commands[8]}")
+		## check samba status
+		if srv_active smbd; then
+			echo "Samba service is active."
+			return 0
+		elif ! srv_enabled smbd; then
+			echo "Samba service is disabled."
+			return 1
+		else
+			echo "Samba service is in an unknown state."
+			return 1
+		fi
+		;;
+		*)
+		echo "Invalid command. Try: '${module_options["module_samba,example"]}'"
+		;;
+	esac
+}
