1c634597761b6aaccfd7da76a2c38927a663711a
Aan <6284204+aancw@users.noreply.github.com>
2025-04-19 13:35:04 +0700

    Switch adguard home to network host for accurate client ip address in logging and dashboard

--- tools/modules/software/module_adguardhome.sh
+++ tools/modules/software/module_adguardhome.sh
@@ -35,7 +35,7 @@ function module_adguardhome () {
 				${module_options["module_adguardhome,feature"]} ${commands[1]}
 			fi
 			docker run -d \
-			--net=lsio \
+			--net=host \
 			-p 53:53/tcp -p 53:53/udp \
 			-p 80:80/tcp -p 443:443/tcp -p 443:443/udp -p 3000:3000/tcp \
 			-p 784:784/udp -p 853:853/udp -p 8853:8853/udp \
