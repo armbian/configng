cc1d4c538bb24ed9c98277f20a2eb5dfefd06909
Serkan Sahin <14278530+SergeantSerk@users.noreply.github.com>
2025-12-20 17:09:35 +0000

    Fix dialog typo

--- tools/modules/system/change_system_hostname.sh
+++ tools/modules/system/change_system_hostname.sh
@@ -10,7 +10,7 @@ module_options+=(
 # @description Change system hostname
 #
 function change_system_hostname() {
-	local new_hostname=$($DIALOG --title "Enter new hostnane" --inputbox "" 7 50 3>&1 1>&2 2>&3)
+	local new_hostname=$($DIALOG --title "Enter new hostname" --inputbox "" 7 50 3>&1 1>&2 2>&3)
 	[ $? -eq 0 ] && [ -n "${new_hostname}" ] && hostnamectl set-hostname "${new_hostname}"
 }
 
