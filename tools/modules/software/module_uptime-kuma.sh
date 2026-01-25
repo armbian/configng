37743586d33f35f9d62452bd889a03617b647f8d
Igor Pecovnik <igor@armbian.com>
2026-01-25 22:41:14 +0100

    Tests: adjust test parameters and broken modules

--- tools/modules/software/module_uptime-kuma.sh
+++ tools/modules/software/module_uptime-kuma.sh
@@ -17,10 +17,9 @@ function module_uptimekuma () {
 	local title="uptimekuma"
 	local condition=$(which "$title" 2>/dev/null)
 
-	if pkg_installed docker-ce; then
-		local container=$(docker container ls -a | mawk '/uptime-kuma?( |$)/{print $1}')
-		local image=$(docker image ls -a | mawk '/uptime-kuma?( |$)/{print $3}')
-	fi
+	pkg_installed docker.io || module_docker install
+	local container=$(docker container ls -a --filter "name=uptime-kuma" --format '{{.ID}}')
+	local image=$(docker image ls -a --format '{{.Repository}} {{.ID}}' | grep 'uptime-kuma '| awk '{print $2}')
 
 	local commands
 	IFS=' ' read -r -a commands <<< "${module_options["module_uptimekuma,example"]}"
@@ -29,7 +28,6 @@ function module_uptimekuma () {
 
 	case "$1" in
 		"${commands[0]}")
-			pkg_installed docker-ce || module_docker install
 			[[ -d "$UPTIMEKUMA_BASE" ]] || mkdir -p "$UPTIMEKUMA_BASE" || { echo "Couldn't create storage directory: $UPTIMEKUMA_BASE"; exit 1; }
 			docker run -d \
 			--net=lsio \
@@ -39,26 +37,28 @@ function module_uptimekuma () {
 			-v "${UPTIMEKUMA_BASE}:/app/data" \
 			louislam/uptime-kuma:1
 			for i in $(seq 1 20); do
-				if docker inspect -f '{{ index .Config.Labels "build_version" }}' uptime-kuma >/dev/null 2>&1 ; then
-					break
-				else
-					sleep 3
+				state="$(docker inspect -f '{{.State.Status}}' uptime-kuma 2>/dev/null || true)"
+				if [[ "$state" == "running" ]]; then
+				break
 				fi
-				if [ $i -eq 20 ] ; then
-					echo -e "\nTimed out waiting for ${title} to start, consult your container logs for more info (\`docker logs uptimekuma\`)"
+				sleep 3
+				if [[ $i -eq 20 ]]; then
+					echo -e "\nTimed out waiting for ${title} to start, consult logs (\`docker logs uptime-kuma\`)"
 					exit 1
 				fi
 			done
 		;;
 		"${commands[1]}")
 			if [[ "${container}" ]]; then
-				docker container rm -f "$container" >/dev/null
-			fi
-			if [[ "${image}" ]]; then
-				docker image rm "$image" >/dev/null
+				echo "Removing container: $container"
+				docker container rm -f "$container"
 			fi
 		;;
 		"${commands[2]}")
+			${module_options["module_uptimekuma,feature"]} ${commands[1]}
+			if [[ "${image}" ]]; then
+				docker image rm "$image"
+			fi
 			${module_options["module_uptimekuma,feature"]} ${commands[1]}
 			if [[ -n "${UPTIMEKUMA_BASE}" && "${UPTIMEKUMA_BASE}" != "/" ]]; then
 				rm -rf "${UPTIMEKUMA_BASE}"
